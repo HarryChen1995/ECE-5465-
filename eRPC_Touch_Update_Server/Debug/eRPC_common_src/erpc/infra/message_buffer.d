d9be3f97095858ef1a054cec50295f1f869896d8
bobataylor <cam@rtastudio.com>
2019-03-07 13:05:25 -0500

    gitignore fix

--- eRPC_Touch_Update_Server/Debug/eRPC_common_src/erpc/infra/message_buffer.d
+++
@@ -1,9 +0,0 @@
-eRPC_common_src/erpc/infra/message_buffer.o \
- eRPC_common_src/erpc/infra/message_buffer.d: \
- ../eRPC_common_src/erpc/infra/message_buffer.cpp \
- ../eRPC_includes/erpc/infra/message_buffer.h \
- ../eRPC_includes/erpc/infra/erpc_common.h
-
-../eRPC_includes/erpc/infra/message_buffer.h:
-
-../eRPC_includes/erpc/infra/erpc_common.h:
